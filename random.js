c5e553357fb88464382cce65332da5ce37328f14
Bryan Hankins <bryanhankins@me.com>
2016-09-05 20:51:23 -0500

    coomplete requirements

--- random.js
+++ random.js
@@ -1,1 +1,24 @@
-console.log("test");
+function imgchange() {
+
+  var myImages1 = new Array();
+  myImages1[1] = "red-arrow-left-pointing-5.png";
+  myImages1[2] = "red-arrow-right-pointing-5 copy.png";
+  var rnd = Math.floor(Math.random() * myImages1.length);
+  if (rnd == 0) {
+    rnd = 1;
+  }
+
+  document.getElementById("gen-img").src = myImages1[rnd];
+
+}
+
+var body = document.getElementById("the-body")
+
+var openingTag = '<div class="something"><p><img id="gen-img" src="red-arrow-left-pointing-5.png"></p>';
+var closingTag = "<div>";
+
+body.innerHTML += openingTag + closingTag;
+
+body.addEventListener("click", function (e) {
+  imgchange()
+})
